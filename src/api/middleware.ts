4715434295d6ca65158435f27d2ffbff19a44337
Abdelrahman Altamimi <tamimi6030@gmail.com>
2025-04-30 22:00:09 +0300

    fix security issues

--- src/api/middleware.ts
+++ src/api/middleware.ts
@@ -1,11 +1,10 @@
 import { Request, Response } from "express";
 import { respondWithError } from "./json.js";
 import { getUser } from "../db/queries/users.js";
-import { User } from "../db/schema.js";
 import { getAPIKey } from "./auth.js";
 
 export function middlewareAuth(
-  handler: (req: Request, res: Response, user: User) => void,
+  handler: () => Promise<void> | void
 ) {
   return async (req: Request, res: Response) => {
     try {
@@ -21,7 +20,7 @@ export function middlewareAuth(
         return;
       }
 
-      handler(req, res, user);
+      await handler();
     } catch (err) {
       respondWithError(res, 500, "Couldn't authenticate user", err);
     }
